11cb3897a76b8f579bab83a83be1e7514cc383e7
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-21 12:01:26 +0100

    structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script >
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+
+  <HelloWorld />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script >
+export default {
+  name: 'HelloWord'
+}
+</script>
+
+<template>
+
+  <h1>INIZIO</h1>
+
+</template>
+
+<style scoped>
+h1 {
+  color: black;
+}
+</style>
