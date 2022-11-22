ade7ce1a4ebe681f97bd23a5396172aa4ec9bccd
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-22 20:49:41 +0100

    position

--- src/components/Position.vue
+++ src/components/Position.vue
@@ -8,14 +8,21 @@ export default {
 
 <template>
     <div class="position">
-
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d999.4241396743371!2d-75.67570707478075!3d45.39617047574581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce05eea825aaab%3A0x5d1d325426cfc369!2sFable%20Care%20Center!5e0!3m2!1sit!2sit!4v1669146464086!5m2!1sit!2sit"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </template>
 
 <style scoped>
 .position {
-    height: 500px;
+    /* height: 500px; */
+    /* width: 100%; */
+    /* background-color: blueviolet; */
+}
+
+iframe {
     width: 100%;
-    background-color: blueviolet;
 }
 </style>
