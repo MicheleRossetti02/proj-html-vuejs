66761d0d139f7a5fba739359d05cde7c6fdf20ed
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-23 12:23:24 +0100

    staff

--- src/components/OurStaff.vue
+++ src/components/OurStaff.vue
@@ -21,29 +21,34 @@ export default {
             </div>
         </div>
         <!-- general info -->
-        <div class="info-staff d-flex">
-            <div class="col-6 title-info">
-                <h3>Our staff consist of three teachers and one assistant with experience in early childhood care</h3>
+        <div class="info-staff d-flex mt-5 mb-4">
+            <div class="col-6 title-info pe-5">
+                <h2>Our staff consist of three teachers and one assistant with experience in early childhood care</h2>
             </div>
-            <div class="text-info col-6">
+            <div class="text col-6 mt-3 ms-3">
                 <p>Praesent arcu gravida vehicula est node maecenas loareet morbi a dosis luctus. Urna eget lacinia
                     eleifend praesent luctus a arcu quis facilisis venenatis. Aenean interdum, nibh vitae sodales, magna
                     ante feugiat elit maecenas.</p>
             </div>
         </div>
 
-        <div class="row">
+        <div class="row mb-5">
             <!-- card teacher -->
-            <div class="col-5" v-for="teacher in store.teacherCard">
+            <div class="col-6" v-for="teacher in store.teacherCard">
 
                 <div class="teacher">
                     <div class="card mb-3">
                         <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="..." class="img-fluid rounded-start" alt="...">
+                            <div class="col-md-6 card-image">
+                                <img :src="`${teacher.img}`" class="img-fluid rounded-start profile" alt="...">
+                                <div class="img-over ">
+                                    <img src="../assets/img/teacher/quote.png" alt="">
+
+                                </div>
                             </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
+
+                            <div class="col-md-6 d-flex align-items-end">
+                                <div class="card-body ">
 
                                     <p class="card-text">{{ teacher.info }}</p>
 
@@ -51,7 +56,7 @@ export default {
                             </div>
                         </div>
                         <div class="row contact-teacher d-flex">
-                            <div class="col">
+                            <div class="col text-center mt-3">
                                 <h5>{{ teacher.name }}</h5>
                                 <p>{{ teacher.role }}</p>
                             </div>
@@ -69,9 +74,66 @@ export default {
     </div>
 </template>
 
-<style scoped >
+<style scoped lang="scss">
+p {
+    opacity: 60%;
+}
+
+p:hover {
+    opacity: 100%;
+}
+
+.info-staff {
+    h2 {
+        color: #554e9e;
+        font-weight: 200;
+
+    }
+}
+
 .row {
     flex-wrap: wrap;
     justify-content: space-between;
+
+    .card {
+        border: 0;
+    }
+
+    .card-image {
+        position: relative;
+
+        .profile {
+            width: 280px;
+            object-fit: cover;
+            height: 250px;
+        }
+    }
+
+    .img-over {
+        position: absolute;
+        right: -7%;
+        top: 20px;
+        padding: 10px;
+        padding-left: 50px;
+        padding-right: 15px;
+        border: 2px solid #fe6601;
+        // bottom: 0;
+        width: fit-content;
+
+        img {
+            width: auto;
+        }
+    }
+
+    .card-body {
+        align-items: baseline;
+    }
+
+    .contact-teacher {
+        h5 {
+            color: #554e9e;
+            font-weight: 300;
+        }
+    }
 }
 </style>
