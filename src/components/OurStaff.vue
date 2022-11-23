943a25b7a0b79ee4f33779bdf65119908cd5998d
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-23 13:10:20 +0100

    complete teacher

--- src/components/OurStaff.vue
+++ src/components/OurStaff.vue
@@ -14,7 +14,7 @@ export default {
     <!-- meet staff -->
     <div class="container mt-5">
         <div class="staff-title text-center">
-            <h1> Meet Our Staff</h1>
+            <h2> Meet Our Staff</h2>
             <p>With education and experience in early childhood care</p>
             <div class="divider">
                 <img src="../assets/img/header_divider.png" alt="">
@@ -39,11 +39,18 @@ export default {
                 <div class="teacher">
                     <div class="card mb-3">
                         <div class="row g-0">
-                            <div class="col-md-6 card-image">
+                            <div class="col-md-5 card-image">
                                 <img :src="`${teacher.img}`" class="img-fluid rounded-start profile" alt="...">
-                                <div class="img-over ">
-                                    <img src="../assets/img/teacher/quote.png" alt="">
+                                <div class="over-position">
+                                    <div class="plus">
+                                        <span class="plus-over">+</span>
 
+                                    </div>
+
+                                    <div class="img-over ">
+                                        <img src="../assets/img/teacher/quote.png" alt="">
+
+                                    </div>
                                 </div>
                             </div>
 
@@ -78,6 +85,11 @@ export default {
 </template>
 
 <style scoped lang="scss">
+h2 {
+    color: #554e9e;
+    font-weight: 300;
+}
+
 p {
     opacity: 60%;
 }
@@ -103,26 +115,56 @@ p:hover {
         border-radius: 0;
     }
 
+    .card-image:hover {
+        span {
+            display: block;
+            cursor: pointer;
+
+        }
+    }
+
     .card-image {
         position: relative;
+        width: fit-content;
 
         .profile {
             width: 280px;
+
             object-fit: cover;
             height: 250px;
         }
     }
 
+
+    span {
+        font-size: 75px;
+        position: absolute;
+        font-weight: 200;
+        top: 15%;
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        left: 25%;
+        opacity: 60%;
+        padding-left: 55px;
+        padding-top: 10px;
+        color: white;
+        background-color: #554e9e;
+        display: none;
+    }
+
+
+
     .img-over {
         position: absolute;
-        right: -7%;
+        right: -20%;
         top: 20px;
         padding: 10px;
         padding-left: 50px;
         padding-right: 15px;
         border: 2px solid #fe6601;
         // bottom: 0;
-        width: fit-content;
+        // width: fit-content;
 
         img {
             width: auto;
